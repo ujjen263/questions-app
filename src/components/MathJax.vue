0e9091713b1fbf1a990562b0e2fec65744340c4c
ChristianVaughn <cvaughn55@gmail.com>
2020-10-06 20:59:30 -0700

    Add asciimath support loose Tex

--- src/components/MathJax.vue
+++ src/components/MathJax.vue
@@ -38,32 +38,8 @@ export default {
     renderMathJax () {
       this.renderContent()
       if (window.MathJax) {
-        window.MathJax.Hub.Config({
-          asciimath2jax: {
-            delimiters: [['`','`'], ['$','$']]
-          },
-          tex2jax: {
-            inlineMath: [['$', '$'], ['(', ')']],
-            displayMath: [['$$', '$$'], ['[', ']']],
-            processEscapes: true,
-            processEnvironments: true
-          },
-
-          // Center justify equations in code and markdown cells. Elsewhere
-          // we use CSS to left justify single line equations in code cells.
-          displayAlign: 'center',
-          'HTML-CSS': {
-            styles: { '.MathJax_Display': { margin: 0 } },
-            linebreaks: { automatic: true }
-          },
-          ...this.options
-        })
         
-        window.MathJax.Hub.Queue([
-          'Typeset',
-          window.MathJax.Hub,
-          this.$refs.mathJaxEl
-        ])
+        window.MathJax.Hub.Queue(["Typeset",window.MathJax.Hub]);
       }
     }
   }
